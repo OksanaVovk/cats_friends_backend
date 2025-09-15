bc9faff9aaf1c5098a8b9393072aebe51200644e
Vovk Oksana <oksvovk85@gmail.com>
2025-09-15 12:52:43 +0300

    Update avatar

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -31,33 +31,44 @@ const avatarsDir = path.join(__dirname, '../../public/avatars');
 
 const updateAvatar = async (req, res) => {
   if (!req.file) {
+    console.log('Файл не надійшов у req.file');
     return res.status(400).json({ message: 'File not provided' });
   }
 
   const { path: tempUpload, originalname } = req.file;
   const { _id: id } = req.user;
 
+  console.log('Отримано файл:', tempUpload, originalname);
+
   try {
     // Створюємо папку avatars, якщо її нема
     await fs.mkdir(avatarsDir, { recursive: true });
+    console.log('Папка avatars перевірена/створена:', avatarsDir);
 
-    // Унікальне ім'я файлу (можеш залишити таке ж, як у multer)
+    // Унікальне ім'я файлу
     const imageName = `${id}_${Date.now()}_${originalname}`;
     const resultUpload = path.join(avatarsDir, imageName);
 
-    // Переміщення файлу
-    await fs.rename(tempUpload, resultUpload);
+    // Копіюємо файл у папку avatars
+    await fs.copyFile(tempUpload, resultUpload);
+    console.log('Файл скопійовано у:', resultUpload);
+
+    // Видаляємо тимчасовий файл
+    await fs.unlink(tempUpload).catch(() => {
+      console.log('Не вдалося видалити тимчасовий файл:', tempUpload);
+    });
 
     // Шлях для фронта
     const avatarUrl = `/avatars/${imageName}`;
+    console.log('Avatar URL для фронта:', avatarUrl);
 
     // Оновлюємо користувача у БД
     await User.findByIdAndUpdate(id, { avatarUrl });
+    console.log('Користувач оновлений у БД');
 
     res.json({ avatarUrl });
   } catch (err) {
-    // Якщо щось пішло не так, видаляємо тимчасовий файл
-    await fs.unlink(tempUpload).catch(() => {});
+    console.error('Помилка при збереженні аватара:', err);
     res.status(500).json({ message: err.message });
   }
 };
